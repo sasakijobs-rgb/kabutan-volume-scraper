2697aca4d1f5eb28356a88e8b61bf96a893951d9
sasakijobs-rgb <sasaki.jobs@gmail.com>
2026-05-25 12:57:25 +0900

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -1,17 +1,6 @@
 # =========================
-# 制御py(ここから他pyを実行する)
+# 制御pyてすと用(ここから他pyを実行する)
 # scraper_test.py
-# →last_data20.csvとtoday_data20.csvを比べる
-# (この２ファイルが同じなら全ての処理を停止する)
-# (前回・今回の１ページ目の内容を出力して比較する）
-# →cleanup.py
-# (ファイルが150個以上は削除)
-# →data2csv.py
-# (株探のモバイル版からデータを取得）
-# →merge.py
-# (最初だけ見出しをセット＆２ファイル目以降はデータのみ)
-# →last_data20.csvを更新
-# (正常終了時のみlast～は更新されます)
 # =========================
 import subprocess
 from check_update import (
@@ -52,42 +41,14 @@ def main():
 
     print("\n===== scraper_test.py START =====\n")
 
-    # 更新チェック
-    if not is_updated():
-
-        print("[ABORT] scraper_test.py 終了")
-
-        return
-
-    # STEP 1
-    print("[STEP 1/3] cleanup.py 実行")
+    # STEP 4
+    print("[STEP 4/4] nikkei_vi_data.py 実行")
     #
-    if not run_py("cleanup.py"):
+    if not run_py("nikkei_vi_data.py"):
     
-        print("[ABORT] cleanup失敗")
-    
-        return
-
-    # STEP 2
-    print("[STEP 2/3] data2csv.py 実行")
-    
-    if not run_py("data2csv.py"):
-    
-        print("[ABORT] data2csv失敗")
+        print("[ABORT] nikkei_vi_data.py 失敗")
     
         return
-
-    # STEP 3
-    print("[STEP 3/3] merge.py 実行")
-
-    if not run_py("merge.py"):
-
-        print("[ABORT] merge失敗")
-
-        return
-
-    # 全成功時のみ更新
-    update_last()
 
     print("\n===== scraper_test.py END =====\n")
 
