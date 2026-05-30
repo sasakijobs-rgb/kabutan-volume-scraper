224ce79cdca50a7ef2fda92e1f03023745dd4db8
sasakijobs-rgb <sasaki.jobs@gmail.com>
2026-05-30 22:59:38 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,22 +14,6 @@
 # →last_data20.csvを更新
 # (正常終了時のみlast～は更新されます)
 # =========================
-# =========================
-# 制御py(ここから他pyを実行する)
-# scraper.py
-# →last_data20.csvとtoday_data20.csvを比べる
-# (この２ファイルが同じなら全ての処理を停止する)
-# (前回・今回の１ページ目の内容を出力して比較する）
-# →cleanup.py
-# (ファイルが150個以上は削除)
-# →data2csv.py
-# (株探のモバイル版からデータを取得）
-# →merge.py
-# (最初だけ見出しをセット＆２ファイル目以降はデータのみ)
-# →last_data20.csvを更新
-# (正常終了時のみlast～は更新されます)
-# =========================
-
 import subprocess
 from check_update import (
     check_update,
@@ -66,9 +50,6 @@ def main():
 
     print("\n===== scraper.py START =====\n")
 
-    # =========================
-    # ★修正ポイント（ここ）
-    # =========================
     if not check_update():
 
         print("[STOP] 変更なし → 全処理停止")
