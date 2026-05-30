576f34dcbe1ac41b935ef7f9a57af63f931ab4ae
sasakijobs-rgb <sasaki.jobs@gmail.com>
2026-05-31 01:05:55 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,38 +1,15 @@
 # =========================
 # 制御py(ここから他pyを実行する)
 # scraper.py
-# →last_data20.csvとtoday_data20.csvを比べる
-# (この２ファイルが同じなら全ての処理を停止する)
-# (前回・今回の１ページ目の内容を出力して比較する）
+# →last_data20.csvと今回の１ページ目を比較して重複は中止）主に土日祝は中止
 # →cleanup.py
 # (ファイルが150個以上は削除)
 # →data2csv.py
 # (株探のモバイル版からデータを取得）
-# →merge.py
-# (最初だけ見出しをセット＆２ファイル目以降はデータのみ)
 # →nikkei_data_vi.csvを更新
 # →supabaseへcsvを反映する
-# →last_data20.csvを更新
-# (正常終了時のみlast～は更新されます)
+# →last_data20.csvを更新(全てが正常終了時のみ)
 # =========================
-# =========================
-# 制御py(ここから他pyを実行する)
-# scraper.py
-# →last_data20.csvとtoday_data20.csvを比べる
-# (この２ファイルが同じなら全ての処理を停止する)
-# (前回・今回の１ページ目の内容を出力して比較する）
-# →cleanup.py
-# (ファイルが150個以上は削除)
-# →data2csv.py
-# (株探のモバイル版からデータを取得）
-# →merge.py
-# (最初だけ見出しをセット＆２ファイル目以降はデータのみ)
-# →nikkei_data_vi.csvを更新
-# →supabaseへcsvを反映する
-# →last_data20.csvを更新
-# (正常終了時のみlast～は更新されます)
-# =========================
-
 import subprocess
 from check_update import (
     check_update,
